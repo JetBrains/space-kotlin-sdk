12d72ab99e1b4a07d1d8be425a43476b6c060d86
denis.zakharov <denis.zakharov@jetbrains.com>
2022-06-02 15:18:03 +0200

    Simplify `Space.appInstallUrl` function: Client Credentials Flow is always turned on. Message authentication with public key is now not appended to the URL as it is the default one (and shorter URL is better).

--- runtime/src/commonMain/kotlin/space/jetbrains/api/runtime/SpaceAppInstallUrl.kt
+++ runtime/src/commonMain/kotlin/space/jetbrains/api/runtime/SpaceAppInstallUrl.kt
@@ -10,15 +10,16 @@ import io.ktor.http.*
  * @param appEndpoint HTTPS url that Space will use to send messages to the app
  * @param state a string that will be passed to the application in `InitPayload` when user installs the app.
  * Allows to track the installation process across different systems while user is redirected in the browser.
- * @param authFlows authentication flows that application will use to access Space API
- * @param authForMessagesFromSpace authentication for messages sent by Space to the app. Recommended value is [AuthForMessagesFromSpace.PUBLIC_KEY_SIGNATURE]
+ * @param authCodeFlow enables Auth Code flow for the application
+ * @param authForMessagesFromSpace authentication for messages sent by Space to the app. Recommended value is [AuthForMessagesFromSpace.PUBLIC_KEY_SIGNATURE].
+ * The [AuthForMessagesFromSpace.PUBLIC_KEY_SIGNATURE] is the default one and is not appended to URL parameters.
  */
 public fun Space.appInstallUrl(
     spaceServerUrl: String,
     name: String,
     appEndpoint: String,
     state: String? = null,
-    authFlows: Set<SpaceAuthFlow> = setOf(SpaceAuthFlow.ClientCredentials),
+    authCodeFlow: AuthCodeFlow? = null,
     authForMessagesFromSpace: AuthForMessagesFromSpace = AuthForMessagesFromSpace.PUBLIC_KEY_SIGNATURE,
 ): String = with(URLBuilder(spaceServerUrl)) {
     path("extensions", "installedApplications", "new")
@@ -27,39 +28,27 @@ public fun Space.appInstallUrl(
     parameters.append("pair", "true")
     parameters.append("endpoint", appEndpoint)
 
-    authFlows.forEach { authFlow ->
-        when (authFlow) {
-            is SpaceAuthFlow.ClientCredentials -> {
-                parameters.append("client-credentials-flow-enabled", "true")
-            }
-            is SpaceAuthFlow.AuthorizationCode -> {
-                parameters.append("code-flow-enabled", "true")
+    if (authCodeFlow != null) {
+        parameters.append("code-flow-enabled", "true")
 
-                val redirectUris = authFlow.redirectUris.joinToString("\n")
-                parameters.append("code-flow-redirect-uris", redirectUris)
+        val redirectUris = authCodeFlow.redirectUris.joinToString("\n")
+        parameters.append("code-flow-redirect-uris", redirectUris)
 
-                if (authFlow.pkceRequired) {
-                    parameters.append("pkce-required", "true")
-                }
-            }
+        if (authCodeFlow.pkceRequired) {
+            parameters.append("pkce-required", "true")
         }
     }
 
     state?.let { parameters.append("state", state) }
 
-    when (authForMessagesFromSpace) {
-        AuthForMessagesFromSpace.PUBLIC_KEY_SIGNATURE -> parameters.append("has-public-key-signature", "true")
-        AuthForMessagesFromSpace.SIGNING_KEY -> parameters.append("has-signing-key", "true")
+    if (authForMessagesFromSpace == AuthForMessagesFromSpace.SIGNING_KEY) {
+        parameters.append("has-signing-key", "true")
     }
 
     build().toString()
 }
 
-public sealed class SpaceAuthFlow {
-    public object ClientCredentials : SpaceAuthFlow()
-    public class AuthorizationCode(internal val redirectUris: List<String>, internal val pkceRequired: Boolean) :
-        SpaceAuthFlow()
-}
+public class AuthCodeFlow(internal val redirectUris: List<String>, internal val pkceRequired: Boolean)
 
 public enum class AuthForMessagesFromSpace {
     PUBLIC_KEY_SIGNATURE,
